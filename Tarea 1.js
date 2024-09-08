3f251a4b59e10059eb68ad777f811b0b79cf0a8d
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-09-08 12:51:30 -0500

    Sebastian Cifuentes Moncada JS

---
+++ Tarea 1.js
@@ -0,0 +1,50 @@
+let nombre = "Sebastian";
+let apellidos = "Cifuentes moncada";
+let pais = "Colombia";
+let ciudad= "Medellin";
+let edad = 17;
+
+
+ const correo = "sebastian.cifuentes055@pascualbravo.edu.co";
+ const nombredegithub = "sebastian055";
+ const universidad = "pascual bravo";
+ const asignatura = "Programacion web";
+ const lenguajeprogramacion = "Java";
+
+
+const comoestas = 0;
+let igualelevado;
+let igualmultiplicacion;
+
+const preguntar = () => "¿Como estas?";
+for (let hola = 0; hola < comoestas; hola++);
+
+const elevaralados = (numero1) => numero1 * numero1;
+switch (numero1) {
+    case 1:
+        igualelevado = numero1 ** 3;
+        break;
+    case 2:
+        igualelevado = numero1 ** 4;
+        break;
+
+        return igualelevado;
+}
+
+const multiplicar = (numero2, numero3) => numero2 * numero3;
+switch (numero2) {
+    case 1:
+        igualmultiplicacion = numero3 * 1;
+    break;
+    case 2:
+        igualmultiplicacion = numero3 * 2;
+    break;
+    case 3:
+        igualmultiplicacion = numero3 * 3;
+    break;
+
+    return igualmultiplicacion;
+}
+
+console.log (preguntar());
+
